b3ea2335e59deda6f46f574d51039046ae01f6d6
niroojane <116459392+niroojane@users.noreply.github.com>
2025-07-25 19:56:42 +0100

    Update Stock_Data.py

--- Stock_Data.py
+++ Stock_Data.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 Niroojane Selvam
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+
 #!/usr/bin/env python
 # coding: utf-8
 
