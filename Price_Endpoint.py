bab350afc1aa113d8e8f6a5b675ddd10ffe79410
niroojane <116459392+niroojane@users.noreply.github.com>
2025-07-25 19:56:15 +0100

    Update Price_Endpoint.py

--- Price_Endpoint.py
+++ Price_Endpoint.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 Niroojane Selvam
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+
 import requests
 import pandas as pd
 import datetime
