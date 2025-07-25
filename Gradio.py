4f98e41e73ee519b1ffd6a37f45cb85ff0e8ad03
niroojane <116459392+niroojane@users.noreply.github.com>
2025-07-25 19:55:43 +0100

    Update Gradio.py

--- Gradio.py
+++ Gradio.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2025 Niroojane Selvam
+# Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+
 import gradio as gr
 from datetime import datetime
 import pandas as pd
