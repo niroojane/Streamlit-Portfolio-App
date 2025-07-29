5e1d0a7513bd56672bdcc300510cf9ecad86af4d
niroojane <116459392+niroojane@users.noreply.github.com>
2025-07-29 19:03:57 +0100

    Update Minimum Variance App.py

--- Minimum Variance App.py
+++ Minimum Variance App.py
@@ -83,6 +83,9 @@ def load_data(tickers,start_date=datetime.datetime(2023,1,1),today=datetime.date
 st.subheader("Constraints")
 
 dataframe,returns_to_use=load_data(tickers=selected,start_date=dt)
+
+st.dataframe(dataframe)
+
 data = pd.DataFrame({'Asset':[None],
 'Sign':[None],
 'Limit':[None]
