4bc727eb84c7b513a024c3b4edc3fffbc71ddf6f
niroojane <116459392+niroojane@users.noreply.github.com>
2026-04-12 16:38:13 +0100

    Add files via upload

--- src/Stock_Data.py
+++ src/Stock_Data.py
@@ -11,46 +11,48 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
 import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def fetch_ticker(ticker, start, end):
+    try:
+        yahoo_data = yf.Ticker(ticker)
+        stock_price = yahoo_data.history(start=start, end=end, interval='1d').reset_index()
+
+        stock_price['Date'] = stock_price['Date'].dt.tz_localize(None)
+        stock_price = stock_price.set_index('Date')
+
+        stock_price[ticker] = stock_price['Close'] + stock_price['Dividends'].shift(periods=-1)
+
+        return stock_price[[ticker]]
+
+    except Exception:
+        return None
 
 
-# In[2]:
+def get_close(
+    tickers,
+    start=datetime.date(datetime.date.today().year - 1,
+                        datetime.date.today().month,
+                        datetime.date.today().day),
+    end=datetime.date.today(),
+    max_workers=10
+):
+    data = pd.DataFrame()
 
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = {
+            executor.submit(fetch_ticker, ticker, start, end): ticker
+            for ticker in tickers
+        }
 
-def get_close(tickers,
-              start=datetime.date(datetime.date.today().year - 1, 
-                datetime.date.today().month, 
-                datetime.date.today().day)
-                ,end=datetime.date.today()):
-    
-    data=pd.DataFrame()
-    for ticker in tickers:
+        for future in as_completed(futures):
+            result = future.result()
 
-        try:
+            if result is not None:
+                data = pd.concat([data, result], axis=1)
 
-            yahoo_data=yf.Ticker(ticker)
-            stock_price=yahoo_data.history(start=start,end=end,interval='1d').reset_index()
-            stock_price['Date']=stock_price['Date'].dt.tz_localize(None)
-            stock_price=stock_price.set_index('Date')
-            stock_price[ticker]=stock_price['Close']+stock_price['Dividends'].shift(periods=-1)
-            data=pd.concat([data,stock_price[ticker]],axis=1)
-
-        except Exception as e:
-            pass
-        
     return data
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
